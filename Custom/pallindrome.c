db9d41a4e8ecdc3bc89aa388cec6601e82748733
Pruthviii18 <pruthvirajnagawade18@gmail.com>
2026-09-12 16:14:20 +0530

    Initial code

---
+++ Custom/pallindrome.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+ int og, rem, n;
+ long long reversed = 0;
+ printf("Enter a number:");
+ scanf("%d",&n);
+ //rejecting negative values
+ if(n<0)
+{
+    printf("The number is not a pallindrome");
+}
+
+}
