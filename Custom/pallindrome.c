603aa92564b13ba2ed0857a92fa8776a0b80e1d5
Pruthviii18 <pruthvirajnagawade@gmail.com>
2026-09-12 20:25:45 +0530

    Merge branch 'main' of https://github.com/Pruthviii18/College-programmes

--- Custom/pallindrome.c
+++ Custom/pallindrome.c
@@ -5,10 +5,25 @@ int main()
  long long reversed = 0;
  printf("Enter a number:");
  scanf("%d",&n);
+ og=n;
  //rejecting negative values
  if(n<0)
 {
     printf("The number is not a pallindrome");
 }
-
+while(n!=0)
+{
+    rem = n%10;
+    reversed = (reversed*10) + rem;
+    n /=10;
+}
+if(og==reversed)
+{
+    printf("The number is a pallindrome");
+}
+else
+{
+ printf("The number is not a pallindrome");   
+}
+return 0;
 }
